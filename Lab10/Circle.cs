d73df4f9293c2ab4b44f442ce62da5eb204c2b80
Dave Bourke <squire40@hotmail.com>
2018-10-20 17:33:33 -0400

    Lab 10 complete

---
+++ Lab10/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab10
+{
+    public class Circle
+    {
+        private double _radius;
+
+        public double Radius
+        {
+            get
+            {
+                return _radius;
+            }
+            set
+            {
+                _radius = value;
+            }
+        }
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double CalculateCirumference()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public string CalculateFormattedCircumference()
+        {
+            return FormatNumber(CalculateCirumference());
+        }
+
+        public double CalculateArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+        public string CalculateFormattedArea()
+        {
+            return FormatNumber(CalculateArea());
+        }
+
+        private string FormatNumber(double x)
+        {
+            return x.ToString("#.00");
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = null;
+            var isValid = false;
+            var shouldContinue = false;
+            double radius = 0;
+            Circle c = null;
+            List<Circle> circles = new List<Circle>();
+
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            do
+            {
+                shouldContinue = true;
+
+                do
+                {
+                    try
+                    {
+                        isValid = true;
+                        Console.WriteLine("Enter radius:");
+                        radius = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Enter a valid value.");
+                        isValid = false;
+                    }
+                } while (!isValid);
+
+                c = new Circle(radius);
+
+                circles.Add(c);
+
+                Console.WriteLine($"Circumference: {c.CalculateFormattedCircumference()}");
+
+                Console.WriteLine($"Area: {c.CalculateFormattedArea()}");
+
+                Console.WriteLine("Continue? y/n");
+
+                shouldContinue = Console.ReadLine().Equals("y", StringComparison.InvariantCultureIgnoreCase);
+
+            } while (shouldContinue);
+
+            Console.WriteLine($"Goodbye.  You created {circles.Count} circles");
+            Console.ReadKey();
+        }
+    }
+}
